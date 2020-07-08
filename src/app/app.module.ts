bbefa3609d3a51e7d9d1a897cb9169d69867b08d
VinchoArtur <a.vincho.work@gmail.com>
2020-07-08 10:03:21 +0300

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import { SeriesListComponent } from './components/series-list/series-list.component';
-import { NamePanelComponent } from './components/name-panel/name-panel.component';
-import { GenrePanelComponent } from './components/genre-panel/genre-panel.component';
-import { PremierYearPanelComponent } from './components/premier-year-panel/premier-year-panel.component';
-import { SeriesViewComponent } from './components/series-view-panel/series-view.component';
+import {SeriesListComponent} from './components/series-list/series-list.component';
+import {NamePanelComponent} from './components/name-panel/name-panel.component';
+import {GenrePanelComponent} from './components/genre-panel/genre-panel.component';
+import {PremierYearPanelComponent} from './components/premier-year-panel/premier-year-panel.component';
+import {SeriesViewComponent} from './components/series-view-panel/series-view.component';
+import {NbIconModule, NbThemeModule, NbLayoutModule, NbFormFieldModule, NbInputModule, NbSelectModule} from '@nebular/theme';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [
@@ -17,14 +20,22 @@ import { SeriesViewComponent } from './components/series-view-panel/series-view.
     NamePanelComponent,
     GenrePanelComponent,
     PremierYearPanelComponent,
-    SeriesViewComponent
+    SeriesViewComponent,
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NbIconModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({name: 'default'}),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbInputModule,
+    NbSelectModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
